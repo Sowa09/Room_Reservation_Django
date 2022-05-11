59265a319230fe59d30643f03b41c6da2761ea1f
Sowa09 <tsowa09@gmail.com>
2022-05-11 20:42:41 +0200

    Add form

--- RoomReservation/views.py
+++ RoomReservation/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+
+from .models import RoomForm
+
+
+def index(request):
+    return render(request, 'base.html')
+
+
+class AddRoomView(View):
+    def get(self, request):
+        form = RoomForm()
+        return render(request, 'add_room.html', {'form': form})
+
+    def post(self, request):
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('add_room'))
+        return render(request, 'add_room.html', {'form': form})
