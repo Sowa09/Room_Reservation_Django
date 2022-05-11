4604b1e939ee7e8d7244157f2de1fd290d0f298d
Sowa09 <tsowa09@gmail.com>
2022-05-11 18:57:54 +0200

    Added magic method __str__

--- room_reservation_django/models.py
+++ room_reservation_django/models.py
@@ -6,6 +6,8 @@ class Room(models.Model):
     capacity = models.PositiveIntegerField()
     projector_availability = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.name} - {self.capacity}'
 
 
 
