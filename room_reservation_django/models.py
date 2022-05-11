61fd2ee885b1355e6a4e9712d906da8899221650
Sowa09 <tsowa09@gmail.com>
2022-05-11 18:51:03 +0200

    Project settings

---
+++ room_reservation_django/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    projector_availability = models.BooleanField(default=False)
+
+
+
+
